ba70a830d0b513cbe50cb9fc506a10c13382afa5
Mishad <hydh95@gmail.com>
2018-09-04 13:20:55 +0300

    Rewrite sql and fix select pidor

--- trash.py
+++ trash.py
@@ -11,17 +11,9 @@ token = misc.token
 CURRENT_OFFSET_FILE = 'updates_id'
 URL = "https://api.telegram.org/bot" + token + "/"
 
-mydb = mysql.connector.connect(
-    host = '127.0.0.1',
-    user = misc.user_db,
-    passwd = misc.password_db,
-    database = 'members'
-)
-mycursor = mydb.cursor()
-
-# def send_message(chat_id, text = 'Wait a second, please...'):
-#     url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
-#     requests.get(url)
+def send_message(chat_id, text = 'Wait a second, please...'):
+    url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
+    requests.get(url)
 
 def insert_into_bd(id, first_name, last_name, chat_id, count_pidor=0):
     mycursor.execute('SELECT id, chat_id FROM pidors;')
@@ -41,12 +33,3 @@ def insert_into_bd(id, first_name, last_name, chat_id, count_pidor=0):
             else:
                 not_found = True
                 send_message(chat_id, 'уже зарегався пидор!')
-
-def select_pidor(chat_id):
-
-    chat_id = (chat_id, )
-    sql = 'SELECT first_name, last_name FROM pidors WHERE chat_id = %s'
-
-    mycursor.execute(sql, chat_id)
-    my_result = mycursor.fetchall()
-    return random.choice(my_result)
--- update.py
+++ update.py
@@ -1,11 +1,18 @@
 from trash import (
     CURRENT_OFFSET_FILE,
-    URL
+    URL, send_message
 )
 import requests, mysql.connector, misc, random
 
 
 class Update:
+    conn = mysql.connector.connect(
+        host = '127.0.0.1',
+        user = misc.user_db,
+        passwd = misc.password_db,
+        database = 'members'
+    )
+    my_cursor = conn.cursor()
     def __init__(self, data):
         self.update_id = data.get("update_id", 0)
         self.chat_id = data.get("chat_id", None)
@@ -14,22 +21,40 @@ class Update:
         self.first_name = data.get("first_name", None)
         self.last_name = data.get("last_name", None)
 
-class MySQL_connection(Update):
-    def __init__(self, chat_id):
-        Update.__init__(self.chat_id)
-        self.conn = mysql.connector.connect(
-            host = '127.0.0.1',
-            user = misc.user_db,
-            passwd = misc.password_db,
-            database = 'members'
-        )
-        self.my_cursor = self.conn.cursor()
-
-    def select_pidor(self):
+    def select_pidor(self, chat_id):
         self.sql = 'SELECT first_name, last_name FROM pidors WHERE chat_id = {}'.format(self.chat_id)
 
         self.my_cursor.execute(self.sql)
         return random.choice(self.my_cursor.fetchall())
 
-connect = MySQL_connection()
-print(connect.select_pidor())
+    def writting_into_db(self, user_id, first_name, last_name, chat_id, count_pidor):
+        self.my_cursor.execute('SELECT id, chat_id FROM pidors;')
+        self.check = (self.user_id, self.chat_id)
+        self.not_found = True
+
+        for x in self.my_cursor.fetchall():
+            if x == self.check:
+                self.not_found = False
+                break
+
+            if self.not_found:
+                self.sql = "INSERT INTO pidors (id, first_name, last_name, chat_id, count_pidor)VALUES (%s, %s, %s, %s, %s)"
+                self.val = (self.user_id, self.first_name, self.last_name, self.chat_id, self.count_pidor)
+                self.my_cursor.execute(self.sql, self.val)
+                self.conn.commit()
+            else:
+                print('hui')
+                send_message(self.chat_id, 'уже зарегався пидор!')
+                self.not_found = True
+
+    def parseTextField(self):
+        if self.text == '/start@local_pidar_bot':
+            self.writting_into_db(self.user_id, self.first_name, self.last_name, self.chat_id, self.count_pidor)
+
+        if self.text == '/action@local_pidar_bot':
+            send_message(
+                self.chat_id,
+                ' '.join(self.select_pidor(self.chat_id))
+            )
+        # with open(CURRENT_OFFSET_FILE, 'w') as f:
+        #     f.write(str(self.update_id + 1))
